c7661fba4ff84a8b1767004df34ff1a44acc29fa
Atharv Naidu <atharvan@berkeley.edu>
2024-11-10 22:34:40 -0800

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,6 +12,7 @@ const Footer = () => {
         <p>Phone: <a href="tel:+16504318235">+1 (650) 431-8235</a></p>
         <p>LinkedIn: <a href="https://www.linkedin.com/in/atharvn/" target="_blank" rel="noopener noreferrer">@atharvn</a></p>
         <p>My Github: <a href="https://github.com/atharvnaidu" target="_blank" rel="noopener noreferrer">Github</a></p>
+        <p>IG/Insta: <a href="https://www.instagram.com/atharvnna/" target="_blank" rel="noopener noreferrer">Instagram</a></p>
         <p>LeetCode: <a href="https://leetcode.com/u/atharvnaidu/" target="_blank" rel="noopener noreferrer">LeetCode Profile</a></p>
         <p>Spotify: <a href="https://open.spotify.com/user/31u6biefskmynlwgx6uuwvjexg2q" target="_blank" rel="noopener noreferrer">Spotify</a></p>
       </div>
